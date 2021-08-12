8c7f88daceb9f4648d8b73d88883b14d446f66df
Boyce Young <54334676+boyceby@users.noreply.github.com>
2021-08-12 16:03:52 -0400

    Consolidate logIn and logOut views.

--- backend/auth.py
+++ backend/auth.py
@@ -22,20 +22,23 @@ def registerUser():
                 'user': user.jsonable()
             }
 
-@bp.route('/sessions', methods=['POST'])
-def logIn():
-    json_data = request.json
-    valid_user = User.verifyValidUser(json_data['username'], json_data['password'])
-    if not valid_user:
-        return make_response("Invalid login credentials.", 400)
-    else:
-        user = User.findByUsername(json_data['username'])
-        session['username'] = user.getUsername()
-        return  {
-                    'logged_in': True,
-                    'status': "created",
-                    'user': user.jsonable()
-                }
+@bp.route('/sessions', methods=['POST', 'DELETE'])
+def logInOut():
+    if request.method == 'POST':
+        json_data = request.json
+        valid_user = User.verifyValidUser(json_data['username'], json_data['password'])
+        if not valid_user:
+            return make_response("Invalid login credentials.", 400)
+        else:
+            user = User.findByUsername(json_data['username'])
+            session['username'] = user.getUsername()
+            return  {
+                        'logged_in': True,
+                        'status': "created",
+                        'user': user.jsonable()
+                    }
+    session.pop('username', default=None)
+    return "Logged out."
 
 @bp.route('/logged_in', methods=['GET'])
 def loggedIn():    
@@ -46,8 +49,3 @@ def loggedIn():
                     'logged_in': True,
                     'user': User.findByUsername(session['username']).jsonable()
                 }
-
-@bp.route('/logout', methods=['DELETE'])
-def logOut():
-    session.pop('username', default=None)
-    return "Logged out."
